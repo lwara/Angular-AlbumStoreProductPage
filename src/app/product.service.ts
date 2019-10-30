1908537b6b65c82780e18d79334fd9d1ce0915c5
Tiwonge Lwara <tlwara@malswitch.mw>
2019-10-30 08:02:31 +0200

    part6BackUp

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -13,6 +13,7 @@ import {Http,Response} from '@angular/http';
 export class ProductService {
   private  _albumUrl='../assets/album.json';
 private _productsUrl='../assets/products.json';
+
   constructor(private _http:Http)
    {
 
@@ -21,8 +22,9 @@ private _productsUrl='../assets/products.json';
   getAlbum(id:number): Observable<Album>{
     return this._http.get(this._albumUrl).map((response)=><Album>response.json());
   }
+
   getProducts():Observable<Product[]>
   {
-    return this._http.get(this._productsUrl).map(response => <Product[]> response.json());
+    return this._http.get(this._productsUrl).map((response) => <Product[]> response.json());
   }
 }
